c7a791d5d2e132921c2edd13882ebda02471b31c
Hannah Eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2023-03-15 12:57:41 +1000

    first comit

---
+++ 01/hello.py
@@ -0,0 +1,8 @@
+# hello.py (filename)
+# Hannah 15.03.2023 (author data)
+# This is my first program that prints "Hello World" (description)
+
+print("Hello World")
+
+msg = "Hello World"
+print(msg)
