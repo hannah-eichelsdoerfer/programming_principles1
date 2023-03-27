7bf4b9aceffe286c8f0a1e414335d72e3f512bb9
Hannah Eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2023-03-28 03:58:58 +1000

    add function

---
+++ 01/add.py
@@ -0,0 +1,10 @@
+# add.py
+# Hannah 15.03.2023
+# This is a program that adds two numbers
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum = num1 + num2
+
+print("The sum of the two numbers is: ", sum)
